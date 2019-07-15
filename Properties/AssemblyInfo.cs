29436728a9ab136860bb33a27f149903319d391f
Jeroen Stemerdink <jeroen.development@outlook.com>
2019-07-15 13:30:29 +0200

    Renamed project to Epi.Extensions.Settings, due to new naming guidelines

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EpiserverExtensions.Settings")]
+[assembly: AssemblyTitle("Epi.Extensions.Settings")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EpiserverExtensions.Settings")]
+[assembly: AssemblyProduct("Epi.Extensions.Settings")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
